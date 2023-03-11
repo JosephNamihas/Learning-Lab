612cab1f70c9922c2669741c8ed92e1f412a13c4
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-11 19:03:30 +0000

    Installed Node. Installed Bootstrap.Installed AXIOS. Integrated API, but getting a 403 error at the moment

---
+++ src/API/DictionaryAPI.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const DictionaryAPI = () => {
+const options = {
+  method: 'GET',
+  url: 'https://dictionary35.p.rapidapi.com/wordSearchEnglish',
+  params: {query: 'book'},
+  headers: {
+    'X-RapidAPI-Key': '45bf6776a6msh463e3442df4ac1cp1653d9jsn85578a44ffc8',
+    'X-RapidAPI-Host': 'dictionary35.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+}
+
+export default DictionaryAPI;
