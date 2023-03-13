bf95965bb36c3d68ccb14dab89702983dcc1e296
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-13 09:53:40 +0000

    API now working. Placed in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import WordsAPI from './API/Words';
-import * as ReactDOM from 'react-dom';
+import axios from "axios";
+import React from "react";
+
+const options = {
+  method: 'GET',
+  url: 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf',
+  headers: {
+    'X-RapidAPI-Key': '958ecc0b13msh0ca9342a3cd9f49p1a6a0bjsnba6be6199123',
+    'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 
 function App() {
   return (
     <div>
-      <WordsAPI />
 
       
     </div>
