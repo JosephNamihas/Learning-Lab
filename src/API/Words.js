fc504bbd339e0ea8614b7d21d0c055a06edd2469
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-13 09:26:42 +0000

    Error checking

--- src/API/Words.js
+++ src/API/Words.js
@@ -5,7 +5,7 @@ function WordsAPI() {
 
 const options = {
   method: 'GET',
-  url: 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf',
+  url: 'https://wordsapiv1.p.rapidapi.com/words/hatchback',
   headers: {
     'X-RapidAPI-Key': '45bf6776a6msh463e3442df4ac1cp1653d9jsn85578a44ffc8',
     'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
