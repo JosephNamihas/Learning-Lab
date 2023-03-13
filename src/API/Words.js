77cb3557576001deb777920575304482370038e7
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-13 09:14:00 +0000

    Added Words API

---
+++ src/API/Words.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+function WordsAPI() {
+
+
+const options = {
+  method: 'GET',
+  url: 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf',
+  headers: {
+    'X-RapidAPI-Key': '45bf6776a6msh463e3442df4ac1cp1653d9jsn85578a44ffc8',
+    'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+}
+
+export default WordsAPI;
