efe34be0160bb74c9e78461dd0b56ea63967520d
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-15 17:04:19 +0000

    Added imports to index.js for Form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import ContactForm from './components/ContactForm';
+import Form from './components/ContactForm';
 
 const root = document.getElementById('root');
 const rootElement = ReactDOM.createRoot(root);
 rootElement.render(
   <React.StrictMode>
-    <ContactForm />
+    <Form />
   </React.StrictMode>
 );
 
