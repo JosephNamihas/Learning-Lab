ab705b7f44a52b9a6275293d05a5e2970abb0c56
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-15 18:04:38 +0000

    update code for one avatar/login fix

--- src/components/LandingPage/AvatarSelector.js
+++ src/components/LandingPage/AvatarSelector.js
@@ -6,71 +6,83 @@ import AvatarLux from '.../avatars/Avatar-Lux.png';
 import AvatarFlash from '.../avatars/Avatar-Flash.png';
 import AvatarNeya from '.../avatars/Avatar-Neya.png';
 import AvatarPeony from '.../avatars/Avatar-Peony.png';
+import AvatarMario from '.../avatars/AvatarMario.png';
+import AvatarZei from '.../avatars/AvatarZei.png';
 
 function AvatarSelector() {
-  const [username, setUsername] = useState(localStorage.getItem('username') || '');
-  const [avatar, setAvatar] = useState(localStorage.getItem('avatar') || '');
+    const [username, setUsername] = useState(localStorage.getItem('username') || '');
+    const [avatar, setAvatar] = useState(localStorage.getItem('avatar') || '');
+    const [inputValue, setInputValue] = useState('');
 
-  const handleLogin = () => {
-    localStorage.setItem('username', username);
-    localStorage.setItem('avatar', avatar);
-  };
+    const handleLogin = () => {
+        setUsername(inputValue);
+        localStorage.setItem('username', inputValue);
+        localStorage.setItem('avatar', avatar);
+    };
 
-  const handleAvatarChange = (e) => {
-    const newAvatar = e.target.value;
-    localStorage.setItem('avatar', newAvatar);
-    setAvatar(newAvatar);
-  };
+    const handleUsernameChange = (e) => {
+        setInputValue(e.target.value);
+    };
 
-  const handleClearStorage = () => {
-    localStorage.clear();
-    setAvatar('');
-    setUsername('');
-  };
+    const handleAvatarChange = (e) => {
+        const newAvatar = e.target.value;
+        localStorage.setItem('avatar', newAvatar);
+        setAvatar(newAvatar);
+    };
 
-  const avatarMap = {
-    'Avatar-Alex': AvatarAlex,
-    'Avatar-Mona': AvatarMona,
-    'Avatar-Hope': AvatarHope,
-    'Avatar-Lux': AvatarLux,
-    'Avatar-Flash': AvatarFlash,
-    'Avatar-Neya': AvatarNeya,
-    'Avatar-Peony': AvatarPeony,
-  };
+    const handleClearStorage = () => {
+        localStorage.clear();
+        setAvatar('');
+        setUsername('');
+    };
 
-  return (
-    <div className="landing-page">
-      <h1>Welcome to the Learning Lab</h1>
-      <div className="avatar-choices">
-        <select onChange={handleAvatarChange} value={avatar}>
-          <option value="">Select an avatar</option>
-          {Object.keys(avatarMap).map(avatarName => (
-            <option key={avatarName} value={`${avatarName}.png`}>{avatarName}</option>
-          ))}
-        </select>
-        {avatar && <img src={avatarMap[avatar.replace('.png', '')]} alt="Selected avatar" />}
-      </div>
-      <div className="login-form">
-        {username ? (
-          <div className="user-info">
-            <p>Logged in as {username}</p>
-            {avatar && <img src={avatarMap[avatar.replace('.png', '')]} alt="Selected avatar" />}
-            <button onClick={handleClearStorage}>Delete Avatar</button>
-          </div>
-        ) : (
-          <>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
-          </>
-        )}
-      </div>
-    </div>
-  );
+    const avatarMap = {
+        'Avatar-Alex': AvatarAlex,
+        'Avatar-Mona': AvatarMona,
+        'Avatar-Hope': AvatarHope,
+        'Avatar-Lux': AvatarLux,
+        'Avatar-Flash': AvatarFlash,
+        'Avatar-Neya': AvatarNeya,
+        'Avatar-Peony': AvatarPeony,
+        'Avatar-Mario': AvatarMario,
+        'Avatar-Zei': AvatarZei
+    };
+
+    return (
+        <div className="landing-page">
+            <h1>Welcome to the Learning Lab</h1>
+            <div className="login-form">
+                {username ? (
+                    <div className="user-info">
+                        <div className="user-box">
+                            <p>{username}</p>
+                            {avatar && <img src={avatarMap[avatar.replace('.png', '')]} alt="Selected avatar" />}
+                        </div>
+                        <button onClick={handleClearStorage}>Delete Avatar</button>
+                    </div>
+                ) : (
+                    <>
+                        <input
+                            type="text"
+                            placeholder="Enter your name"
+                            value={inputValue}
+                            onChange={handleUsernameChange}
+                        />
+                        <button onClick={handleLogin}>Login</button>
+                    </>
+                )}
+            </div>
+            <div className="avatar-choices">
+                <select onChange={handleAvatarChange} value={avatar}>
+                    <option value="">Select an avatar</option>
+                    {Object.keys(avatarMap).map(avatarName => (
+                        <option key={avatarName} value={`${avatarName}.png`}>{avatarName}</option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
 }
+
 
 export default AvatarSelector;
