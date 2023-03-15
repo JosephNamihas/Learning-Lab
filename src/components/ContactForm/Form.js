fe84b06527dbd892362b12da03b9812f64e358a1
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-15 16:17:35 +0000

    Added code for contact form

---
+++ src/components/ContactForm/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Future development. Here, we can add code to send the form data to a server.
+    // For this example, we'll just clear the form inputs.
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>We are always looking for ways to serve you better.</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
