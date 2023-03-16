7159e82989b3446332848fccbfdc77824d86a2a5
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-03-16 22:32:38 +0000

    Added/ updated home page Navbar and CSS

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,34 +7,65 @@ function Home() {
 	return (
 		<section className={styles.home_section} id="home">
       <header className='Home-header'>
-        <Container>
-          <Row>
-          <Col md>
-            <p className={styles.greet}>Hi there, welcome to</p>
+        <Container className="text-center">
+          <Card.Body>
+          <p className={styles.greet}>Hi there, welcome to</p>
 			      <h1 className={styles.heading_1}>Learning Lab.</h1>
 			      <h1 className={styles.heading_2}>To Help Your Learning Journey Continue Uninterrupted</h1>
-			      <a href="#journal">
-				      <button className={styles.btn}>Sign in !</button>
-			      </a>
+          </Card.Body>
+          <Button variant="outline-success">Awesome Things Here !</Button>
+          
+          <Row>
+            <Col md-3>
+              <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="./images/math.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card tite
+                </Card.Text>
+                <Button variant="success">Go somewhere</Button>
+              </Card.Body>
+              </Card>
             </Col>
-            <Col md>
-              <Card className='mb-3'>
-                <Card.Img src='./bg2.png' />
-               </Card>
+            <Col md-3>
+              <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="./images/English.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card tite
+                </Card.Text>
+                <Button variant="success">Go somewhere</Button>
+              </Card.Body>
+              </Card>
             </Col>
-            
+            <Col md-3>
+              <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="./images/more.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card tite
+                </Card.Text>
+                <Button variant="success">Go somewhere</Button>
+              </Card.Body>
+              </Card>
+            </Col>
           </Row>
-          <Card className="text-center">
+
+         
+        </Container>
+        <Card className="text-center">
           <Card.Body>
             <Card.Title>Special title treatment</Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="success">Go somewhere</Button>
           </Card.Body>
           <Card.Footer className="text-muted">Footer</Card.Footer>
         </Card>
-        </Container>
       </header>
 		</section>
 	);
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -1,28 +1,23 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import HomeIcon from '@mui/icons-material/Home';
 import HelpIcon from '@mui/icons-material/Help';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import FontDownloadIcon from '@mui/icons-material/FontDownload';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+
+
 
 function Collapsible() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Learning-Lab</Navbar.Brand>
+        <Navbar.Brand href="#home">LEARNING_LAB</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Math</Nav.Link>
-            <Nav.Link href="#pricing">Vocabuluary</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features"><CalculateIcon /></Nav.Link>
+            <Nav.Link href="#pricing"><FontDownloadIcon /></Nav.Link>
+            <Nav.Link href="#pricing"><NewspaperIcon /></Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets"> <HomeIcon /> </Nav.Link>
