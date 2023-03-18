7475e429f52c11c9817c63f8167a026d85173a7b
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-18 10:27:18 +0000

    Error check

--- src/App.js
+++ src/App.js
@@ -9,21 +9,6 @@ import Vocabulary from './components/LearningPage/Vocabulary';
 import Footer from './components/Shared/Footer/Footer';
 // import Planner from "./components/Journal/Planner";
 
-{/*const options = {
-  method: "GET",
-  url: "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
-  headers: {
-    "X-RapidAPI-Key": "958ecc0b13msh0ca9342a3cd9f49p1a6a0bjsnba6be6199123",
-    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});*/}
-
 function App() {
   return (
       <div className="App">
